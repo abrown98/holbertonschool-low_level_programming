1ecb27dc1ef2ddf855a4cb6f5cca609fb4b0798b
abrown98 <4929@holbertonschool.com>
2022-05-23 13:59:58 -0700

    review printing integers

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar (x);
+	}
+	putchar ('\n');
+	return (0);
+}
