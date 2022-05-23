9f151af481f99663a65358f0e2ebd5027842a21c
abrown98 <4929@holbertonschool.com>
2022-05-23 14:02:35 -0700

    review functions

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar (x % 10 + '0');
+	}
+	for (y = 'a'; y <= 'f'; y++)
+	{
+		putchar (y);
+	}
+	putchar ('\n');
+
+	return (0);
+}
