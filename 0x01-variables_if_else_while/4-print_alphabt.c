cc4352c671d269bee7c5ac5ddb46de3e820c8189
abrown98 <4929@holbertonschool.com>
2022-05-23 13:53:00 -0700

    print a..z

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 'e' && x != 'q')
+		{
+		putchar(x);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
