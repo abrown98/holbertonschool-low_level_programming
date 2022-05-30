b7da4f6c9f885972280670e684d55d4cf95f9b98
abrown98 <4929@holbertonschool.com>
2022-05-30 12:38:16 -0700

    unfinished

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ *
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ *
+ */
+
+void positive_or_negative(int i); 
+
+#endif /* MAIN_H */
---
+++ 0x03-debugging/practiceagain.c
@@ -0,0 +1,21 @@
+int main( void )
+{
+	/* Set a and b both equal to 5 */
+
+	a = b = 5;
+
+	/* Print them, decremting each time. */
+	/* Use prefix mode for b, postfix mode for a */
+
+	printf("\nPost  Pre");
+	printf("\n%d");
+	printf("\n%d");
+
+	return 0;
+
+
+
+
+
+}
+
