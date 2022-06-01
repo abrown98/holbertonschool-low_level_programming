d2ba360f4154470b55c9f0dd45bc5f828318e120
root <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>
2022-05-31 21:51:47 -0700

    ayayy

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - print a diagonal line in term
+ * @n: how long the line will be
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int d;
+	int s;
+
+	if (n > 0)
+	{
+		for (d = 0; d < n; d++)
+		{
+			for (s = 0; s < d; s++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
