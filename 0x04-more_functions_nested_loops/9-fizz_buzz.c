cce5794679188c1a580c65f6a55c7fa682b53b48
root <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>
2022-05-31 21:56:57 -0700

    good

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print Fizz, or, Buzz, or FizzBuzz
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+	int Fizz;
+	int Buzz;
+
+	for (num = 1; num < 100; num++)
+	{
+		Fizz = num % 3 == 0;
+		Buzz = num % 5 == 0;
+		if (Fizz && Buzz)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (Fizz)
+		{
+			printf("Fizz ");
+		}
+		else if (Buzz)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+		printf("%d ", num);
+		}
+	}
+	printf("Buzz");
+	printf("\n");
+	return (0);
+}
