c407439c37ac050b4a583e6351c26b50849a7f0d
root <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>
2022-05-31 21:19:57 -0700

    igotthis

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiply two intergers.
+ * @a: fist interger
+ * @b: second interger
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
