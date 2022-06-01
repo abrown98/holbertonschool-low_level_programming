b3fed5588ee79fe2a9c1a05a69fd5bd7e3e91bd7
root <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>
2022-05-31 21:54:24 -0700

    im doing this

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - print a x by x square wherr x is input
+ * @size: size of the square
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int row;
+	int colomn;
+
+	if (size > 0)
+	{
+		for (colomn = 0; colomn < size; colomn++)
+		{
+			for (row = 0; row < size; row++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
