601d7eed1c9b96a9a1a982637a446ea08db747dd
root <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>
2022-06-02 21:54:43 -0700

    stuff

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,20 @@
 #include "main.h"
-
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * _puts - check code
- *
- * @str: interger input
- *
+ *_puts - prints a string, to stdout
+ *@str: value to be evaluate.
+ *Return: not.
  */
 void _puts(char *str)
 {
-	_putchar(*str);
+	int l = 0;
+
+	while (*(str + l) != '\0')
+	{
+		_putchar(str[l]);
+		l++;
+	}
+	_putchar('\n');
 }
