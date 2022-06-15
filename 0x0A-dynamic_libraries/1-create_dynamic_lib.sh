a8b06771332dc4dca11bd5837118e5068e90d285
root <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>
2022-06-15 13:31:52 -0700

    here

--- 0x0A-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x0A-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gcc *.c -c -fPIC liball.so
+gcc -c *.c
+gcc -shared -o liball.so *.o
---
+++ 0x0A-dynamic_libraries/time.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+	int age;
+
+	printf("Please enter your age: ");
+	scanf("%d", &age);
+	
+	if(age < 100) {
+	printf(You are pretty young!\n");
+	}
+	else if(age == 100) {
+	printf("You are old\n");
+	}
+	else {
+		p
