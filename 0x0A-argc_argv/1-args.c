bad48a0b1288ddc863239cec5b6e5d116fd392c7
root <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>
2022-06-14 17:12:09 -0700

    here

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,17 @@
-#include <stdio.h>
 #include "main.h"
-
+#include <stdio.h>
+/**
+ * main - prints the number of arguments.
+ * @argc: argument count
+ * @argv: Argument vector for values
+ *
+ *
+ * Return: Always to 0
+ */
 int main(int argc, char *argv[])
 {
-
 	(void)argv;
 	argc--;
 	printf("%d\n", argc);
-	return 0;
+	return (0);
 }
