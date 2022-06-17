699e7e6c39bdf91ea7f5aadd01529e55b5358097
root <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>
2022-06-16 21:20:12 -0700

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+
+	char *array;
+
+	unsigned int index;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = c;
+
+	return (array);
+}
