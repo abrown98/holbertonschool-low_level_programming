cb59b8d43b808f8a56b84607351621682c45cfc6
root <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>
2022-06-21 17:33:55 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,20 +1,30 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+
+/**
+  * _calloc - allocates memory for an array of nmemb elements of
+  *           size bytes each and returns a pointer to the allocated memory
+  * @nmemb: number of elements
+  * @size: byte size of each array element
+  *
+  * Return: NULL if nmemb = 0, size = 0, or function fails
+  *         pointer to allocated memory otherwise
+  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *mem;
 	char *fluff;
 	unsigned int index;
-	
-	if(nmemb == 0 || size == 0)
+
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	mem = malloc(size * nmemb);
 
 	if (mem == NULL)
 		return (NULL);
-	
+
 	fluff = mem;
 
 	for (index = 0; index < (size * nmemb); index++)
