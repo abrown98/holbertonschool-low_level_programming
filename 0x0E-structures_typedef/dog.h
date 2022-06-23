eb2b7ebae95e3640f3b979d276d4a8d769632071
root <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>
2022-06-22 18:12:13 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,22 @@
-#ifndef _DOG_H_
-#define _DOG_H_
-
+#ifndef DOG_H
+#define DOG_H
+/*end of include guard*/
 /**
- * dog_t - Typedef for struct dog
+ * struct dog - create a dog
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
  */
-typedef struct dog dog_t;
-
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
+} dog_t;
+/*end of structure*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
-#endif
+/*end of prototypes*/
+#endif/*DOG_H*/
