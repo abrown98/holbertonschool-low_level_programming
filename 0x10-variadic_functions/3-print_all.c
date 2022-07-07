084b7baf780c07021fb9255b6197ff45353f2b0e
root <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>
2022-07-06 21:56:10 -0700

    print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,79 +1,20 @@
 #include "variadic_functions.h"
-/**
- * print_char - print characters
- * @arguments: input arguments
- */
-void print_char(va_list arguments)
-{
-	printf("%c", va_arg(arguments, int));
-}
-/**
- * print_int - print integers
- * @arguments: input arguments
- */
-void print_int(va_list arguments)
-{
-	printf("%d", va_arg(arguments, int));
-}
-/**
- * print_float - print floats
- * @arguments: input arguments
- */
-void print_float(va_list arguments)
-{
-	printf("%f", va_arg(arguments, double));
-}
-/**
- * print_string - print strings
- * @arguments: input arguments
- */
-void print_string(va_list arguments)
-{
-	char *args = va_arg(arguments, char*);
 
-	if (args == NULL)
-	{
-		printf("%p", args);
-		return;
-	}
-	printf("%s", args);
+void ch_print(va_list, c)
+
+{
+	printf("%c", va_arg(c, char);
 }
-/**
- * print_all - prints anything
- * @format: input string
- */
+
+void int_print(va_list numb)
+{
+	printf("%i", va_arg(i, int);
+}
+void float_print(va_list, deci)
+{
+	printf("%f", va_arg(d, double);
+}
 void print_all(const char * const format, ...)
 {
-	simbol_t identifier[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'f', print_float},
-		{'i', print_int}
-	};
-
-	int i = 0, j;
-	char *comma = "";
-
-	va_list arguments;
-
-	va_start(arguments, format);
-
-	while (format && format[i])
-	{
-		j = 0;
-
-		while (j < 4)
-		{
-			if (identifier[j].all == format[i])
-			{
-				printf("%s", comma);
-				identifier[j].func(arguments);
-				comma = ", ";
-			}
-			j++;
-		}
-		i++;
+	va_list args
 	}
-	printf("\n");
-	va_end(arguments);
-}
