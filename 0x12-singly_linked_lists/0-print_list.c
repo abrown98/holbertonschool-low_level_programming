3e403ae88e254b33e674076535e25801a69a0543
root <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>
2022-07-08 21:28:09 -0700

    printf

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	list_t *head;
+	list_t hello = {"World", 5, NULL};
+	list_t *new;
+	size_t h;
+
+	head = &goodbye;
+	new = malloc(sizeof(list_t));
+	if (str == NULL)
+	{
+		printf("[0] (nil)\n");
+		return (1);
+	}
+	new->len = 10;
+	new->str = strdup("Hello");
+	new->next = head;
+	printf("-> %lu elements\n", n);
+
+	free(new -> str);
+	return 0;
+}
